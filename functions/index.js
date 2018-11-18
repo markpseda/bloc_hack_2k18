72ce98e33c1f5a4a3449a7fd7631993ee78a6e60
Mark Seda <markpseda@gmail.com>
2018-11-17 23:35:48 -0500

    Added a thing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+ });
+
+ exports.tester = functions.database.ref(`test`)
+    .onCreate((snap, context)=>{
+        console.log("here1");
+        let data = snap.val();
+        console.log(data);
+        return;
+    });
